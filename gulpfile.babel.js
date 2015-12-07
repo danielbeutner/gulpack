6125bbc1102d7390b1e39285ebe7bb54ea218c59
Daniel Ditgens <daniel@ditgens.net>
2015-12-07 16:49:58 +0100

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+import gulp from 'gulp';
+import runSequence from 'run-sequence';
+
+/* Importing all task we need */
+// import
+
+gulp.task('default', cb => {
+  // runSequence('clean', cb);
+});
